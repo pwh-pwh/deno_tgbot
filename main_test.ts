3d3ba44536accff2fa87be2717c36cdefeacba7c
pwh-pwh <plmoknpwh@163.com>
2025-08-08 14:26:35 +0800

    update test

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,8 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+
+function add(a:number,b:number):number {
+  return a + b;
+}
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
